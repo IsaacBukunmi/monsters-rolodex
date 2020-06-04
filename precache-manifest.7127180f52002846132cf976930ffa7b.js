ec763a62c226388978ca0e131a0c83b99c99b8a0
Isaac <adetonaisaac12345@gmail.com>
2020-06-04 14:35:48 +0100

    Updates

---
+++ precache-manifest.7127180f52002846132cf976930ffa7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0152db84de528e76128bdad7f3d49811",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "00f5c3fd713c68c50245",
+    "url": "/monsters-rolodex/static/css/main.39579ae2.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00f5c3fd713c68c50245",
+    "url": "/monsters-rolodex/static/js/main.9d25fa00.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
